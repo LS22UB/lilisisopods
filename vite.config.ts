7f94bd14b4a40f7afde16421cd71ce642a2a6d48
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-07-17 20:53:26 +0200

    path corrected

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default () => {
     build: {
       rollupOptions: {
         input: {
-          app: './src/index.html',
+          app: './public/index.html',
         },
       },
       outDir: "dist",
