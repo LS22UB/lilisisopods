26a227e38d0e9befdc1b3966654ca733ef073da5
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-07-17 18:53:09 +0200

    added

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default () => {
+  const env = loadEnv("dev", process.cwd());
+  return defineConfig({
+    base: `.`,
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+    },
+    publicDir: 'public',
+  });
+  
+};
