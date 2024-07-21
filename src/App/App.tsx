ed884f090c438033cf9ac3cdd1e93a1095d73668
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-07-21 18:00:04 +0200

    unused import

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { NotFound } from "../components/NotFound";
 import { Navbar } from "../components/Navbar";
