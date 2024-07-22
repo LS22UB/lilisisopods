324dd0d7b9d35cd6630377fe7f8137a11f64d6ce
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-07-22 23:15:25 +0200

    auskommentiert

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -4,10 +4,10 @@ export function SocialRow(){
     return(
         <div className="center">
             <div className="row align-items-center">
-                <div className="col-2 center"><a href="#" className="fa fa-reddit" property="url"></a></div>
-                <div className="col-2 center"><a href="#" className="fa fa-envelope" property="url"></a></div>
-                <div className="col-2 center"><a href="#" className="fa fa-instagram" property="email"></a></div>
-                <div className="col-2 center"><a href="#" className="fa fa-github" property="email"></a></div>
+                <div className="col-2 center"><a href="https://www.reddit.com/user/hurly_burly_pegasus/?rdt=59692" className="fa fa-reddit" property="url"></a></div>
+                <div className="col-2 center"><a href="mailto:lilisisopods@proton.me" className="fa fa-envelope" property="url"></a></div>
+                <div className="col-2 center"><a href="https://www.instagram.com/lilisisopods/" className="fa fa-instagram" property="email"></a></div>
+                {/* <div className="col-2 center"><a href="#" className="fa fa-github" property="email"></a></div> */}
             </div>
         </div>
         
