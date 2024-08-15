3dce8b3eff666b541bf25d0cc7f6dc4514070be1
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-08-15 14:11:29 +0200

    fix Anchors must have content and the content must be accessible by a screen reader

--- src/components/Embeds/components/embeds/RedditEmbed.tsx
+++ src/components/Embeds/components/embeds/RedditEmbed.tsx
@@ -32,7 +32,7 @@ export function RedditEmbed({url}:{url: string}){
     
         return(
             <div className='col reddit'>
-            <blockquote className="reddit-embed-bq"  data-embed-height="740"><a href={url}></a><br/> <a href=""></a> in <a href="https://www.reddit.com/r/isopods/">isopods</a></blockquote>
+            <blockquote className="reddit-embed-bq"  data-embed-height="740"><a href={url}></a><br/> <a href="https://www.reddit.com/r/isopods/"></a> in <a href="https://www.reddit.com/r/isopods/">isopods</a></blockquote>
         </div>
         )
 }
