bcc8e37c7624625498dbd575b618a8fcb1654ed5
liliana <liliana.sanfilippo@uni-bielefeld.de>
2024-08-28 21:19:23 +0200

    maybe fixed Unexpected control character(s) in regular expression: \x00, \x1f                                          no-control-regex

--- src/components/Embeds/components/placeholder/PlaceholderEmbed.tsx
+++ src/components/Embeds/components/placeholder/PlaceholderEmbed.tsx
@@ -7,7 +7,7 @@ import { EngagementIconsPlaceholder } from './parts/EngagementIconsPlaceholder';
 import { ProfilePlaceholder } from './parts/ProfilePlaceholder';
 
 // See: https://pragmaticwebsecurity.com/articles/spasecurity/react-xss-part1
-const isJavaScriptProtocol =/^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
+const isJavaScriptProtocol =/^[\u0000-\u001F\ ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
 
 export interface PlaceholderEmbedProps extends DivProps {
   url: string;
